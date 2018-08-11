148baee818fe0b8a5108903287f61458df17dcc0
Gerardo Arjona <gerardo.arj15@gmail.com>
2018-08-10 20:52:40 -0500

    Initial commit
    Signed-off-by: Gerardo Arjona <gerardo.arj15@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DigitalDesignPractice0';
+  title = 'Diseño Digital Practica 0';
 }
